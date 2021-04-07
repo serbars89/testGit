55ff6c80c7582a71fb0df1f4dc2d36863a79ea80
Ser <velikiybarsovich@gmail.com>
2021-04-07 04:10:38 +0300

    add new classes

---
+++ git_tst/src/main/java/TestGit/run2/Print1.java
@@ -0,0 +1,12 @@
+package java.TestGit.run2;
+
+public class Print1 {
+
+    public void text1(){
+        System.out.println("Hello method text1 ");
+    }
+
+    public void text2(){
+        System.out.println("Hello method text2 ");
+    }
+}
